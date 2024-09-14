f251bedcf9672c2a42effdc3ab86c432fcd30f5d
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-09-14 14:50:27 -0700

    Timestamp

--- Lab6.cpp
+++ Lab6.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+// #include <iomanip>
 using namespace std;
 
 void PopulateArray(double *&, int); // Populate array with user input
-void OutputArray(const double *&, int);  // Output array on one line
-double SumArray(const double *&, int);   // Return array sum
+void OutputArray(double *&, int);  // Output array on one line
+double SumArray(double *&, int);   // Return array sum
 
 int main()
 {
@@ -11,41 +12,29 @@ int main()
     int ARRAY_SIZE = 5;
     double *dArrayPtr = new double[ARRAY_SIZE]; // Allocate array of 5 ints
 
-    // Console print before swap
-    // cout << "x = " << x << "  | y = " << y << endl;
-    cout << "xPtr = " << *xPtr << "  | yPtr = " << *yPtr << endl;
-
-    cout << "Swapping...\n";
-    // int sum = swap(x, y);
-    int sum = swap(xPtr, yPtr); // Swap pointers
-
-    // Console print after swap
-    // cout << "x = " << x << " | y = " << y << endl;
-    cout << "xPtr = " << *xPtr << "  | yPtr = " << *yPtr << endl;
-    cout << "sum = " << sum << endl;
+    PopulateArray(dArrayPtr, ARRAY_SIZE); // Call function to populate array
+    OutputArray(dArrayPtr, ARRAY_SIZE); // Call to output array
 
     // Deallocate heap elements and nullify pointers
-    delete xPtr, yPtr;
-    xPtr = nullptr;
-    yPtr = nullptr;
+    delete [] dArrayPtr;
+    dArrayPtr = nullptr;
 
     return 0;
 }
 
 // Populate array with user input
-void PopulateArray(double *&arrPtr, int arrSize)
+void PopulateArray(double *& arrPtr, int arrSize)
 {
     // For array size, loop with cin to get double values
     for (size_t i = 0; i < arrSize; i++)
     {
-        cout << endl
-             << "Please enter a double: ";
+        cout << "Please enter a double: ";
         cin >> *(arrPtr + i);
     }
 }
 
 // Output array on one line
-void OutputArray(const double *&arrPtr, int arrSize)
+void OutputArray(double *& arrPtr, int arrSize)
 {
     // For array size, output each value in some kind of formatted way
     cout << "Array: {";
@@ -58,7 +47,13 @@ void OutputArray(const double *&arrPtr, int arrSize)
 }
 
 // Return array sum
-double SumArray(const double *&arrPtr, int arrSize)
+double SumArray(double *& arrPtr, int arrSize)
 {
     // Just return of the sum of the darn thing
+    double sum = 0.0;
+    for (size_t i = 0; i < arrSize; i++)
+    {
+        sum += *(arrPtr + i);
+    }
+    return sum;
 }
