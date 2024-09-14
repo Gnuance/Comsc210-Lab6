0357e326b268e4756869032bec294ade73751e70
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-09-14 15:03:28 -0700

    Timestamp

--- Lab6.cpp
+++ Lab6.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 void PopulateArray(double *&, int); // Populate array with user input
-void OutputArray(double *&, int);  // Output array on one line
-double SumArray(double *&, int);   // Return array sum
+void OutputArray(double *&, int);   // Output array on one line
+double SumArray(double *&, int);    // Return array sum
 
 int main()
 {
@@ -12,12 +12,12 @@ int main()
     int ARRAY_SIZE = 5;
     double *dArrayPtr = new double[ARRAY_SIZE]; // Allocate array of 5 ints
 
-    PopulateArray(dArrayPtr, ARRAY_SIZE); // Call function to populate array
-    OutputArray(dArrayPtr, ARRAY_SIZE); // Call to output array
-    cout << "Sum = " << fixed << setprecision(2) << SumArray(dArrayPtr, ARRAY_SIZE) << endl; // Output sum
+    PopulateArray(dArrayPtr, ARRAY_SIZE);                                                    // Call function to populate array
+    OutputArray(dArrayPtr, ARRAY_SIZE);                                                      // Call to output array
+    cout << "Sum = " << fixed << setprecision(2) << SumArray(dArrayPtr, ARRAY_SIZE) << endl; // Output sum formatted
 
     // Deallocate heap elements and nullify pointers
-    delete [] dArrayPtr;
+    delete[] dArrayPtr;
     dArrayPtr = nullptr;
 
     return 0;
@@ -27,28 +27,31 @@ int main()
 void PopulateArray(double *& arrPtr, int arrSize)
 {
     // For array size, loop with cin to get double values
+    cout << "Data entry to populate array:" << endl;
     for (size_t i = 0; i < arrSize; i++)
     {
-        cout << "Please enter a double: ";
+        cout << "\t> Please enter a double for index #" << i << ": ";
         cin >> *(arrPtr + i);
     }
+    cout << "Data entry complete." << endl;
 }
 
 // Output array on one line
-void OutputArray(double *& arrPtr, int arrSize)
+void OutputArray(double *&arrPtr, int arrSize)
 {
     // For array size, output each value in some kind of formatted way
     cout << "Array: {";
     for (size_t i = 0; i < arrSize; i++)
     {
-        if (i > 0) cout << ", "; // Add ',' and ' ' for each additional value after the first
+        if (i > 0)
+            cout << ", "; // Add ',' and ' ' for each additional value after the first
         cout << *(arrPtr + i);
     }
     cout << "}" << endl;
 }
 
 // Return array sum
-double SumArray(double *& arrPtr, int arrSize)
+double SumArray(double *&arrPtr, int arrSize)
 {
     // Just return of the sum of the darn thing
     double sum = 0.0;
